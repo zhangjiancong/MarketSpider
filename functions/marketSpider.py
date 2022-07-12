08ffaba7c6be4f138e7784a03334c17a3f8e287b
张健聪 <zhangjiancong1@126.com>
2022-07-12 18:22:12 +0800

    更新HAR

--- functions/marketSpider.py
+++ functions/marketSpider.py
@@ -1,8 +1,10 @@
 import csv
 import json
+import sys
 import time
-
-import winsound
+import tkinter
+from threading import Thread
+from tkinter import filedialog, messagebox
 from playsound import playsound
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -64,3 +66,65 @@ class browserObj:
 
     def close_exit(self):
         self.browserWin.close()
+
+
+class MyGUI():
+    def __init__(self,title='MarketSpider 状态监视工具'):
+        self.root = tkinter.Tk()
+        self.root.title(title)
+        self.root['background'] = '#ffffff'
+        self.root.geometry('600x150-50+20')
+        self.root.attributes("-topmost", -1)
+        self.statue = tkinter.Label(self.root, text='初始化', font=('微软雅黑', '18'), background='#e3e3e3',
+                                    wraplength=195)
+        self.statue.place(x=0, y=0, height=150, width=200)
+        self.btn_store = tkinter.Button(self.root, text='保存信息(F8)')
+        self.btn_store.place(x=230, y=65, height=30, width=100)
+        self.btn_next = tkinter.Button(self.root, text='下一个店铺(F9)')
+        self.btn_next.place(x=342, y=65, height=30, width=100)
+        self.btn_1 = tkinter.Button(self.root, text='备用')
+        self.btn_1.place(x=454, y=65, height=30, width=100)
+        self.total = tkinter.Label(self.root, text='已完成:  -  总计:  -', font=('微软雅黑', '10'), background='#ffffff')
+        self.total.place(x=230, y=9, height=20)
+        self.label_1 = tkinter.Label(self.root, text='', font=('微软雅黑', '10'), background='#ffffff')
+        self.label_1.place(x=230, y=37, height=20)
+
+    def run(self):
+        # Gui_thread = Thread(target=self.root.mainloop, daemon=True)
+        # Gui_thread.start()
+        self.root.mainloop()
+
+    def autowork(self):
+        self.statue['background'] = '#fffa65'
+        self.statue['text'] = f'自动化操作中'
+
+    def needcode(self, shopid):
+        self.statue['background'] = '#7bed9f'
+        self.statue['text'] = f'请输入验证码!\n店:{shopid}'
+
+    def warning(self, reason):
+        self.statue['background'] = '#ff4757'
+        self.statue['text'] = f'出错\n异常:{reason}'
+
+    def progresscontrol(self, now, total):
+        self.total['text'] = f'已完成： {now}   总计： {total}'
+
+    def custom_statue(self, text, color='#e3e3e3'):
+        self.statue['text'] = text
+        self.statue['background'] = color
+
+    def custom_label0(self, text, color='#FFFFFF'):
+        self.total['text'] = text
+        self.total['background'] = color
+
+    def custom_label1(self, text, color='#FFFFFF'):
+        self.label_1['text'] = text
+        self.label_1['background'] = color
+
+    def open_file_dialog(self,title='请打开CSV文件'):
+        filePath=tkinter.filedialog.askopenfilename(title=title)
+        if filePath=='':
+            tkinter.messagebox.showerror('错误','您没有打开任何CSV文件')
+            sys.exit()
+        return filePath
+
---
+++ pdd_HAR_reader.py
@@ -0,0 +1,53 @@
+import csv
+import json
+
+har=input('HAR_File:')
+import time
+har_file = open(har, 'r', encoding='utf-8')
+har_file = har_file.read()
+har_json = json.loads(har_file)
+j_ahead = har_json['log']['entries']
+nums = 0
+
+
+csv_file=open(f'PDD-{time.strftime("%Y-%m-%d_%H-%M", time.localtime())}.csv','a', encoding='utf-8-sig', newline='')
+csv_writer=csv.DictWriter(csv_file,fieldnames=['goods_id','goods_name','goods_pic','mall_id','price','has_saled'])
+csv_writer.writerow({'goods_id':'商品ID','goods_name':'商品名','goods_pic':'商品图片','mall_id':'店铺ID','price':'价格','has_saled':'参考销售量'})
+csv_file.flush()
+
+def read_json(Reqtext):
+    json_data = json.loads(Reqtext)
+    json_data = json_data['items']
+    for good in json_data:
+        info = good['item_data']['goods_model']
+        try:
+            ss = info['sales']
+        except:
+            try:
+                ss = info['sales_tip']
+            except:
+                ss = '未知'
+        csv_writer.writerow(
+            {'goods_id': info['goods_id'], 'goods_name': info['goods_name'], 'goods_pic': info['hd_thumb_url'],
+             'mall_id': info['mall_id'], 'price': info['price_info'],
+             'has_saled': ss})
+        csv_file.flush()
+
+
+
+for reqs in j_ahead:
+    if reqs["_resourceType"]=='xhr':
+        if 'proxy/api/search?' in reqs['request']['url']:
+            nums=nums+1
+            print(nums)
+            try:
+                a = reqs['response']['content']['text']
+                read_json(a)
+            except:
+                continue
+
+print('========遍历完成===========')
+csv_file.flush()
+csv_file.flush()
+csv_file.flush()
+time.sleep(3)
